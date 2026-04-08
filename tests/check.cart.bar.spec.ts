688e45d6f65d96ef8417b308f5b786e48aa41ac4
melnykvadim <vadym.melnyk111@gmail.com>
2026-04-08 15:39:25 +0200

    return changes

--- tests/check.cart.bar.spec.ts
+++ tests/check.cart.bar.spec.ts
@@ -4,7 +4,6 @@ import { PageManager } from '../page-objects/page.manager';
 const baseURL = process.env.URL as string;
 
 test.beforeEach(async ({ page }) => {
-    console.log("baseUrl: " + baseURL)
     await page.goto(baseURL + 'moisturizer');
 
 })
@@ -14,14 +13,14 @@ test('page has correct title', async ({ page }) => {
     await expect(pm.onProductsPage().pageTitleLocator).toHaveText('Moisturizers')
 })
 
-// test('cart is empty', async ({ page }) => {
-//     const pm = new PageManager(page)
-//     await expect(pm.onProductsPage().cartlocator).toHaveText('Cart - Empty')
-// })
+test('cart is empty', async ({ page }) => {
+    const pm = new PageManager(page)
+    await expect(pm.onProductsPage().cartlocator).toHaveText('Cart - Empty')
+})
 
-// test('cart has item(s)', async ({ page }) => {
-//     const pm = new PageManager(page)
-//     const cheapestProduct = await pm.onProductsPage().getCheapestProductByName('aloe')
-//     await pm.onProductsPage().clickAddBtn(cheapestProduct.index)
-//     await expect(pm.onProductsPage().cartlocator).toHaveText('Cart - 1 item(s)')
-// })
+test('cart has item(s)', async ({ page }) => {
+    const pm = new PageManager(page)
+    const cheapestProduct = await pm.onProductsPage().getCheapestProductByName('aloe')
+    await pm.onProductsPage().clickAddBtn(cheapestProduct.index)
+    await expect(pm.onProductsPage().cartlocator).toHaveText('Cart - 1 item(s)')
+})
